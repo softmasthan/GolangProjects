7d8c5f7e9a9e430ef562d78d572f9b527101a8e5
softmasthan <65585319+softmasthan@users.noreply.github.com>
2020-05-23 21:39:11 +0530

    comments changes
    only comments changed

--- ftp.go
+++ ftp.go
@@ -10,31 +10,34 @@ import (
 
 func main() {
 	//Referred site:- https://github.com/jlaffaye/ftp
-	//Connecting Ftp Details with username and password
+	
+	//Initializing IP Address with port 21 Ex:- 127.0.0.1:21
 	c, err := ftp.Dial("ipAdress:21", ftp.DialWithTimeout(5*time.Second))
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
+	//Connecting Ftp Details with username and password
 	err = c.Login("username", "password")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//Give local path drive where ftp.zip fiel is present
+	//Give full local path of a drive, where your file is present  Ex: F:\\ftp.zip
 	sourceFile, err := os.Open("F:\\ftp.zip")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer sourceFile.Close()
 
-	// Do something with the FTP conn
-	//Remote ftp.zip file has going to move in server
+	// Do something with the FTP connection
+	//Moving a ftp.zip file from local PC to Remote Server or some other FTP connected system
 	err = c.Stor("ftp.zip", sourceFile)
 	if err != nil {
 		panic(err)
 	}
 
+	//Closing the connection of a ftp
 	if err := c.Quit(); err != nil {
 		log.Fatal(err)
 	}
